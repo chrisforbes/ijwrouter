2fad33b9d1710fe05c5854c63c4dbefc709a273d
chrisf <chrisf@6ac832f5-8447-0410-b697-eb102b644f48>
2008-03-03 10:56:43 +0000

    git-svn-id: svn://svn.ijw.co.nz/ijw-router/trunk@3 6ac832f5-8447-0410-b697-eb102b644f48

---
+++ src/common.h
@@ -0,0 +1,23 @@
+/*
+	defines core types & macros
+*/
+
+typedef unsigned char u08;
+typedef unsigned short u16;
+typedef unsigned long u32;
+typedef unsigned __int64 u64;
+
+typedef signed char i08;
+typedef signed short i16;
+typedef signed long i32;
+typedef signed __int64 i64;
+
+/* on win32, hal declarations are function pointers.
+   on arm9 target, hal declarations are just normal functions. */
+
+#ifdef WIN32
+#define HAL(f)	(*f)
+#else
+#define HAL(f)	f
+#endif
+
