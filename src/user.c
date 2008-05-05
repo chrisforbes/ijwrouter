2ec049ad9c6a04ff05352ab35ef99ebf444bf3b3
beedee <beedee@6ac832f5-8447-0410-b697-eb102b644f48>
2008-05-05 04:08:42 +0000

    fixes bugs #1, #2
    git-svn-id: svn://svn.ijw.co.nz/ijw-router/trunk@97 6ac832f5-8447-0410-b697-eb102b644f48

--- src/user.c
+++ src/user.c
@@ -20,7 +20,7 @@ user users[] =
 {
 	{ "odd-socks", 0, 0 },
 	{ "other-woman", 104857600u, 1073741824u },
-	{ "laptop-0", 0, 0 },
+	{ "laptop-0", 0, 104857600u },
 	{ "laptop-1", 0, 104857600u },
 };
 
@@ -32,7 +32,7 @@ user * get_user_by_ip( u32 addr )
 	if (addr == inet_addr( "192.168.2.8" ))
 		return &users[1];
 
-	if (addr == inet_addr( "192.168.0.106" ))
+	if (addr == inet_addr( "192.168.2.9" ))
 		return &users[2];
 
 	if (addr == inet_addr( "192.168.0.105" ))
