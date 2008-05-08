31b371425beb7df1e668b07574f4dec646ed7697
chrisf <chrisf@6ac832f5-8447-0410-b697-eb102b644f48>
2008-05-08 02:07:39 +0000

    fix for #26
    git-svn-id: svn://svn.ijw.co.nz/ijw-router/trunk@102 6ac832f5-8447-0410-b697-eb102b644f48

--- src/hal_time.h
+++ src/hal_time.h
@@ -2,5 +2,7 @@
 #define HAL_TIME_H
 
 u32 ticks( void );
+u32 get_time( void );
+void set_time( u32 time );
 
 #endif
--- src/win32/hal_time.c
+++ src/win32/hal_time.c
@@ -1,6 +1,24 @@
 #include "../common.h"
 #include "../hal_time.h"
 
+#define _USE_32BIT_TIME_T
+
 #include <windows.h>
+#pragma warning( disable: 4133 )
+#pragma warning( disable: 4244 )
+#include <time.h>
 
 u32 ticks(void) { return GetTickCount(); }
+
+static u32 time_offset = 0;
+
+u32 get_time(void)
+{
+	return (u32)time(0) + time_offset;
+}
+
+void set_time( u32 time )
+{
+	u32 t = get_time();
+	time_offset = time - t;
+}
